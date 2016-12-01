e5b8102162d84b24bb1ff9c7306d41d601d7cf79
Hawkingtale <mhac5527@740s-pl-173.local>
2016-12-01 13:34:01 -0700

    Base controller is set up

---
+++ src/PokemonFrame/PokemonFrame.java
@@ -0,0 +1,13 @@
+package PokemonFrame;
+
+import javax.swing.*;
+import pokemonController.PokemonModel;
+import pokemonController.PokemonController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PokemonFrame extends JPanel
+{
+
+}
